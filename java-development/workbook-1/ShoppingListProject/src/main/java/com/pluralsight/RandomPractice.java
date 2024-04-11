1fe82abbfc5c712fe7e29c91fe12f1efaa874644
samatisz <shelbymatisz@gmail.com>
2024-04-11 13:55:16 -0400

    initial commit

---
+++ java-development/workbook-1/ShoppingListProject/src/main/java/com/pluralsight/RandomPractice.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class RandomPractice {
+    String word1 = "Hello";
+    String word2 = "World";
+    String greeting = word1 + " " + word2 + "!";
+
+}
+public static void Main(String[] args) {
+
+}
+
+
---
+++ java-development/workbook-1/ShoppingListProject/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("eggs, cheese, english muffins, tea, rice, avocado, bird food, coffee, grapes, apples");
+
+    }
+}
+
