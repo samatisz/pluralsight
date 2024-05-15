78dbc1efeb6e157cab6b50f5ffd39c8e78abbfde
samatisz <shelbymatisz@gmail.com>
2024-05-15 11:33:26 -0400

    worked on excercises

---
+++ java-development/workbook-5/AssestManager/src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Vehicle extends Asset{
+    private String makeModel;
+    private int year;
+    private int odometer;
+
+    public Vehicle(String description, String dateAcquired, double originalCost, String makeModel, int year, int odometer) {
+        super(description, dateAcquired, originalCost);
+        this.makeModel = makeModel;
+        this.year = year;
+        this.odometer = odometer;
+    }
+}
